4f936ddd3b6572d10563c7a38e0c8f9ef94a4f8b
MuhammadHassan25 <hassan.bscs25@gmail.com>
2021-09-13 03:44:33 +0500

    Work Accomplish

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main() {
+  var a = 32;
+
+  if (a >= 80) {
+    print("Grade :A");
+  } else if (a >= 70) {
+    print("Grade : A");
+  } else if (a >= 60) {
+    print("Grade : B");
+  } else if (a >= 50) {
+    print("Grade : C");
+  } else if (a >= 40) {
+    print("Grade : D");
+  } else if (a >= 33) {
+    print("Grade : E");
+  } else {
+    print("Grade : F -- Fail");
+  }
+}
